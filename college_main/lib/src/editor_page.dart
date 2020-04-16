b6033039005991f0cd2b5efb0be5870cb3d53fa6
Adhyayan Panwar <nullvoiddeath@gmail.com>
2020-04-16 10:06:20 +0530

    Added flutter code

---
+++ college_main/lib/src/editor_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+            child : Container(
+            width: 320,
+            padding: EdgeInsets.all(10.0),
+            child: TextField(
+            autocorrect: true,
+            decoration: InputDecoration(
+              hintText: 'Enter Your Email Here...',
+              prefixIcon: Icon(Icons.email),
+              hintStyle: TextStyle(color: Colors.grey),
+              filled: true,
+              fillColor: Colors.white70,
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                borderSide: BorderSide(color: Colors.green, width: 2),
+               ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                borderSide: BorderSide(color: Colors.green, width: 2),
+              ),
+            ),)
+          ))
+        )
+    );
+  }
+} 
